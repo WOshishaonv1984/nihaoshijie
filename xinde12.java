f1c259d5d9a6f90917e43f6ba20ff743b8bfa143
wenhao1984 <dana521csa15@gmail.com>
2023-04-08 11:38:43 +0900

    test

---
+++ xinde12.java
@@ -0,0 +1,23 @@
+public class xinde12 {
+	
+	public static void main(String[]args) {
+		boolean a=true;
+		if(a==true) {
+			System.out.println("考试通过恭喜");
+			
+			
+			
+		}else {
+			
+			System.out.println("考试没通过 下次努力");
+			
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+}
